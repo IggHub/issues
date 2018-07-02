9a817e528acd8af1057c97c30aece642b1a414e9
Iggy <igoririanto@rocketmail.com>
2018-07-02 13:16:17 -0500

    fix handle_response pattern matching

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -2,6 +2,7 @@ defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
   def fetch(user, project) do
+    IO.puts "Running fetch"
     issues_url(user, project)
       |> HTTPoison.get(@user_agent)
       |> handle_response
@@ -11,6 +12,12 @@ defmodule Issues.GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: {:ok, body}
-  def handle_response(%{status_code: __, body: body}), do: {:error, body}
+  def handle_response({:ok, %{body: body, status_code: 200}}) do
+    IO.puts "running handle_response"
+    {:ok, body}
+  end
+
+  def handle_response({_, %{status_code: __, body: body}}) do
+    {:error, body}
+  end
 end
