d392acb06ff477f7749b8a8798c84466907492f9
apolloguest <nithya.sam@mrcooper.com>
2018-07-02 14:40:06 -0500

    add sorting - need to fix test

---
+++ test/cli_test.exs
@@ -0,0 +1,14 @@
+defmodule CliTest do
+  import Issues.CLI, only: [parse_args: 1, sort_into_ascending_order: 1, convert_to_list_of_hashdicts: 1]
+  test "sort ascending orders the correct way" do
+    result = sort_into_ascending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: issue["created_at"]
+    assert issues == ~w{a b c}
+  end
+
+  defp fake_created_at_list(values) do
+    data = for value <- values,
+      do: [{"created_at", value}, {"other_data", "xxx"}]
+    convert_to_list_of_hashdicts data
+  end
+end 
